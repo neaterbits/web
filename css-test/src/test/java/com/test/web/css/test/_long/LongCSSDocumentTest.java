bb9ca247f09c78ed7f56a3f45386e48aff99a5e6
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2018-07-31 19:57:27 +0200

    Compile test

--- css-test/src/test/java/com/test/web/css/test/_long/LongCSSDocumentTest.java
+++ css-test/src/test/java/com/test/web/css/test/_long/LongCSSDocumentTest.java
@@ -4,7 +4,7 @@ import com.test.web.css._long.LongCSSDocument;
 import com.test.web.css.test.BaseCSSDocumentTest;
 import com.test.web.parse.css.ICSSDocumentParserListener;
 
-public class CSSDocumentTest extends BaseCSSDocumentTest<Integer, Void> {
+public class LongCSSDocumentTest extends BaseCSSDocumentTest<Integer, Void> {
 
 	@Override
 	protected ICSSDocumentParserListener<Integer, Void> createDocument() {
